f1b3902144fb5ae9ad601268e9e7352fd7e4a3b7
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-06-14 18:08:37 -0400

    Conexión con base de datos remota y corrección de errores

--- src/main/java/com/auth/microservicio_autenticacion/security/JwtService.java
+++ src/main/java/com/auth/microservicio_autenticacion/security/JwtService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Service
